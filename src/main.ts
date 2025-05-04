abe5be687d1546139869940a91be015fb19ea99b
Rishabh sharma <rishabh.sharma@antiersolutions.com>
2025-05-04 15:12:47 +0530

    ride

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as fs from "fs";
 import * as express from 'express';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { config } from 'dotenv';
-import * as bodyParser from 'body-parser';
 config()
 const PORT = process.env.PORT || 3001; 
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule,{
+    logger: ['debug', 'error', 'log', 'verbose', 'warn']
+  });
   app.enableVersioning({ type: VersioningType.URI }); // URL versioning e.g., /v1/auth
   app.enableCors();
   app.use(express.json());
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true, skipMissingProperties: true  }));
-  app.useGlobalPipes(new ValidationPipe({ skipMissingProperties: true, transform: true, whitelist: true }));
   const config = new DocumentBuilder()
     .setTitle('Goodcitizen App')
     .setDescription('Goodcitizen App API description')
